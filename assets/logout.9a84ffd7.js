25094faaab9504379d43f470c3ac77c8986c0287
VasiliiPolshakov <45201a@gmail.com>
2023-01-23 07:21:09 +0300

    v.0.2.0

---
+++ assets/logout.9a84ffd7.js
@@ -0,0 +1,1 @@
+import{d as o,G as s,b as e,f as a,o as n,a as t}from"./index.f17c8be4.js";const c={class:"not-found"},_=t("h2",null,[t("div",{class:"title"},"Logout")],-1),r=[_],h=o({__name:"logout",setup(d){return a().dispatch(s.LOGOUT),(l,u)=>(n(),e("div",c,r))}});export{h as default};
