4c4d7bcd13d1cd327ec4dfd29972a2fc157d0eb4
VasiliiPolshakov <45201a@gmail.com>
2024-01-24 01:13:20 +0300

    v0.3.5

---
+++ assets/js/interactive.d4e9a208.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./TopNav.9e79f16f.js";import{_ as n,d as a,b as r,p as s,a as e,r as p,o as c}from"./index.f6ce8b56.js";const i=a({components:{TopNav:o},setup(){return{}}}),d=e("div",{style:{"min-height":"800px","padding-top":"30px"}},[e("iframe",{width:"100%",height:"800vh",src:"/catalog/index.html",frameborder:"0"},`\r
+		`)],-1);function _(m,f,l,h,v,u){const t=p("top-nav");return c(),r("div",null,[s(t,{newOrder:""}),d])}var g=n(i,[["render",_]]);export{g as default};
