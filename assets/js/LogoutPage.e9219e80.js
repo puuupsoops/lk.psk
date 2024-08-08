111375da6d2ba725bfa6c3f2d04b473556af4b8c
Vasilii Polshakov <45201a@gmail.com>
2024-08-08 16:15:59 +0300

    v0.4.0

---
+++ assets/js/LogoutPage.e9219e80.js
@@ -0,0 +1,1 @@
+import{d as o,I as e,b as s,g as a,o as n,a as t}from"./index.6405f1f2.js";const c={class:"not-found"},_=t("h2",null,[t("div",{class:"title"},"Logout")],-1),r=[_],h=o({__name:"LogoutPage",setup(d){return a().dispatch(e.LOGOUT),(u,l)=>(n(),s("div",c,r))}});export{h as default};
