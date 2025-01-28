8c4096c1cd923248cfefb32df108a25be09bead8
Vasilii Polshakov <45201a@gmail.com>
2025-01-28 20:31:29 +0300

    v0.4.1

---
+++ assets/js/interactive.c83d1710.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./TopNav.b4bc3b25.js";import{f as n,d as a,b as r,e as s,a as e,r as c,o as p}from"./index.319ff497.js";const i=a({components:{TopNav:o},setup(){return{}}}),d=e("div",{style:{"min-height":"800px","padding-top":"30px"}},[e("iframe",{width:"100%",height:"800vh",src:"/catalog/index.html",frameborder:"0"},`\r
+		`)],-1);function _(m,f,l,h,v,u){const t=c("top-nav");return p(),r("div",null,[s(t,{newOrder:""}),d])}var g=n(i,[["render",_]]);export{g as default};
